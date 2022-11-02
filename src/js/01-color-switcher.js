65a12ad62a1272ad8b7d6c399a84cfa749bb7168
Iaroslav Seniuk <savuk7@icloud.com>
2022-11-02 17:40:16 +0200

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtnRef = document.querySelector('button[data-start]');
+const stopBtnRef = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let interId = null;
+
+function changeBodyColor() {
+  interId = setInterval(() => {
+    stopBtnRef.parentNode.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  stopBtnRef.removeAttribute('disabled');
+  startBtnRef.setAttribute('disabled', true);
+}
+
+function stopChangeBodyColor() {
+  clearInterval(interId);
+  startBtnRef.removeAttribute('disabled');
+  stopBtnRef.setAttribute('disabled', true);
+}
+
+startBtnRef.addEventListener('click', changeBodyColor);
+stopBtnRef.addEventListener('click', stopChangeBodyColor);
